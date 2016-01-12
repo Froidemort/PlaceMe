99d6b404192ade61d0c362dc8d80660951364bd8
Froidemort <jb.myst89@gmail.com>
2016-01-12 01:25:43 +0100

    Implementation of the _checkDatas method _ToPyObject function used (not opti) Some minor other changes on comments

--- ModelGuest.py
+++ ModelGuest.py
@@ -8,6 +8,8 @@ from StackBar import StackBar
 
 # CONSTANTS (preceded with 'C_') --------------------------------------
 C_MAN, C_WOMAN, C_CHILDREN = tuple(range(3))
+
+
 # FUNCTIONS --------------------------------------------------------------------
 def skip_duplicates(iterable, key=lambda x: x):
     fingerprints = set()
@@ -16,36 +18,41 @@ def skip_duplicates(iterable, key=lambda x: x):
         if fingerprint not in fingerprints:
             yield x
             fingerprints.add(fingerprint)
+def _toPyObject(elem):
+    try:
+        return elem.toPyObject()
+    except AttributeError:
+        return elem
 
 # CLASSES ----------------------------------------------------------------------
 class GuestsModel(QtCore.QAbstractTableModel):
     def __init__(self, parent=None):
-        super(GuestsModel,self).__init__(parent)
+        super(GuestsModel, self).__init__(parent)
         self.parent = parent
         self.guestList = []
         self.headerLabels = ['Name', 'Surname', 'Gender', 'Tags...']
-        #In english for the moment,
-        #need to implement QtLinguist functionnalities
+        # In english for the moment,
+        # need to implement QtLinguist functionnalities
 
     def columnCount(self, parent=QtCore.QModelIndex()):
-        #Fixed to 4 (surname, name, gender, tags)
+        # Fixed to 4 (surname, name, gender, tags)
         return 4
 
     def rowCount(self, parent=QtCore.QModelIndex()):
         return len(self.guestList)
 
-    def data(self, index, role = QtCore.Qt.DisplayRole):
+    def data(self, index, role=QtCore.Qt.DisplayRole):
         if role == QtCore.Qt.DisplayRole:
             if index.column() != 3:
                 return self.guestList[index.row()][index.column()]
             else:
-                #ignore tags for the moment, will be implemented later
+                # ignore tags for the moment, will be implemented later
                 return None
         if role == QtCore.Qt.EditRole:
             if index.column() != 3:
                 return self.guestList[index.row()][index.column()]
             else:
-                #ignore tags for the moment, will be implemented later
+                # ignore tags for the moment, will be implemented later
                 return None
 
     def setData(self, index, value, role=QtCore.Qt.EditRole):
@@ -54,61 +61,61 @@ class GuestsModel(QtCore.QAbstractTableModel):
         if role == QtCore.Qt.EditRole:
             if index.column() != 3:
                 oldValue = self.guestList[index.row()][index.column()]
-                self.guestList[index.row()][index.column()]=value
+                self.guestList[index.row()][index.column()] = value
                 if not self._checkDatas():
-                    QtGui.QMessageBox.warning(self, 'Error : two identical guests',
+                    QtGui.QMessageBox.warning(None, 'Error : two identical guests',
                                               'Two guest cannot be identical')
-                    self.guestList[index.row()][index.column()]=oldValue
+                    self.guestList[index.row()][index.column()] = oldValue
                 else:
                     return True
 
             else:
-                #ignore tags for the moment, will be implemented later
-                self.guestList[index.row()][index.column()]=None
+                # ignore tags for the moment, will be implemented later
+                self.guestList[index.row()][index.column()] = None
             return True
         else:
             return False
 
     def flags(self, index):
-        return QtCore.Qt.ItemIsEnabled |\
-            QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEditable
+        return QtCore.Qt.ItemIsEnabled | \
+               QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEditable
 
     def insertRows(self, row, count, parent=QtCore.QModelIndex()):
         print 'adding row n', row
         self.beginInsertRows(parent, row, row + count - 1)
-        self.guestList.append([None,None,0, None])
-        #assuming count=1
+        self.guestList.append([None, None, 0, None])
+        # assuming count=1
         self.endInsertRows()
 
     def removeRows(self, row, count, parent=QtCore.QModelIndex()):
         print 'deleting row n', row
         self.beginRemoveRows(parent, row, row + count - 1)
-        if len(self.guestList)!=0: self.guestList.pop(row)
+        if len(self.guestList) != 0: self.guestList.pop(row)
         self.endRemoveRows()
 
     def headerData(self, section, orientation, role=QtCore.Qt.DisplayRole):
-        if role == QtCore.Qt.DisplayRole and\
-           orientation == QtCore.Qt.Horizontal:
+        if role == QtCore.Qt.DisplayRole and \
+                        orientation == QtCore.Qt.Horizontal:
             return self.headerLabels[section]
         return QtCore.QAbstractTableModel.headerData(self, section,
-                                                         orientation, role)
+                                                     orientation, role)
+
     def _checkDatas(self):
-        partialGuestList = [elem[:-1] for elem in self.guestList[:] 
+        partialGuestList = [elem[:-1] for elem in self.guestList[:]
                 if elem not in [[None, None, i, None] for i in range(3)]]
-        partialGuestList = [tuple(x) for x in partialGuestList]
-        skippedGuestList = list(skip_duplicates(partialGuestList, lambda x: tuple(x)))
-        print partialGuestList
-        print skippedGuestList
-        if len(partialGuestList)!=len(skippedGuestList):
-            return False
-        else:
-            return True
-            
+        partialGuestList = [tuple([_toPyObject(elem)
+                                   for elem in x])
+                                   for x in partialGuestList]
+        skippedGuestList = list(skip_duplicates(partialGuestList,
+                                lambda x: tuple([_toPyObject(elem)
+                                for elem in x])))
+        return len(partialGuestList)==len(skippedGuestList)
+
 
 class ComboDelegate(QtGui.QItemDelegate):
     def __init__(self, parent):
         self.pathIcons = map(lambda path: QtGui.QIcon(path),
-        ['images/male.png', 'images/female.png', 'images/baby.png'])
+                             ['images/male.png', 'images/female.png', 'images/baby.png'])
         QtGui.QItemDelegate.__init__(self, parent)
 
     def createEditor(self, parent, option, index):
@@ -119,7 +126,7 @@ class ComboDelegate(QtGui.QItemDelegate):
             item.setTextAlignment(QtCore.Qt.AlignHCenter)
             comboModel.appendRow(item)
         self.connect(combo, QtCore.SIGNAL("currentIndexChanged(int)"),
-                         self, QtCore.SLOT("currentIndexChanged()"))
+                     self, QtCore.SLOT("currentIndexChanged()"))
         return combo
 
     def setEditorData(self, editor, index):
@@ -137,54 +144,58 @@ class ComboDelegate(QtGui.QItemDelegate):
 
 class GuestsView(QtGui.QTableView):
     def __init__(self, parent=None):
-        super(GuestsView,self).__init__(parent)
-        #Delegate for gender
+        super(GuestsView, self).__init__(parent)
+        # Delegate for gender
         self.setItemDelegateForColumn(2, ComboDelegate(self))
 
+
 class Widget(QtGui.QWidget):
-        def __init__(self, parent=None):
-            QtGui.QWidget.__init__(self, parent)
-            #Setting the model/view and linking the two.
-            self.model= GuestsModel()
-            self.view = GuestsView()
-            self.view.setModel(self.model)
-            self.view.setSelectionMode(
-                                     QtGui.QAbstractItemView.ExtendedSelection)
-            #Creation of the stackbar, adding button to it.
-            self.gStackBar = StackBar()
-            self.gStackBar.addItem(35,'Add guest', 'images/plus.png', False,
-            'Add a guest at the end of the list')
-            self.gStackBar.addItem(35,'Del guest', 'images/minus.png', False,
-            '''Delete a selected guest.
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        # Setting the model/view and linking the two.
+        self.model = GuestsModel()
+        self.view = GuestsView()
+        self.view.setModel(self.model)
+        self.view.setSelectionMode(
+                QtGui.QAbstractItemView.ExtendedSelection)
+        # Creation of the stackbar, adding button to it.
+        self.gStackBar = StackBar()
+        self.gStackBar.addItem(35, 'Add guest', 'images/plus.png', False,
+                               'Add a guest at the end of the list')
+        self.gStackBar.addItem(35, 'Del guest', 'images/minus.png', False,
+                               '''Delete a selected guest.
             If any are selected, delete the last row''')
-            self.gStackBar.addItem(55, 'Import\nguests',
-            'images/import.png', False,
-            'Import a list of guest from file')
-            self.gStackBar.addItem(55, 'Export\nguests',
-            'images/export.png', False,
-            'Export a list of guest to a file')
-            #Setting function ton the stackbar buttons
-            self.gStackBar.setFunction(0,self.addGuest)
-            self.gStackBar.setFunction(1,self.delGuest)
-            #Creating the vertical layout
-            layout=QtGui.QVBoxLayout()
-            layout.addWidget(self.gStackBar)
-            layout.addWidget(self.view)
-            self.setLayout(layout)
-            #Temporary dimension of the widget.
-            self.setGeometry(300,300,500,500)
-        def addGuest(self):
-            print "Creating a row..."
-            self.model.insertRows(self.model.rowCount(), 1)
-            self.view.openPersistentEditor(
-                            self.model.index(self.model.rowCount()-1, 2))
-        def delGuest(self):
-            print "Deleting a list of selected rows"
-            selection = self.view.selectionModel()
-            l =  reversed(list(skip_duplicates([item.row()
-                                for item in selection.selectedIndexes()])))
-            for row in l:
-                self.model.removeRows(row,1)
+        self.gStackBar.addItem(55, 'Import\nguests',
+                               'images/import.png', False,
+                               'Import a list of guest from file')
+        self.gStackBar.addItem(55, 'Export\nguests',
+                               'images/export.png', False,
+                               'Export a list of guest to a file')
+        # Setting function ton the stackbar buttons
+        self.gStackBar.setFunction(0, self.addGuest)
+        self.gStackBar.setFunction(1, self.delGuest)
+        # Creating the vertical layout
+        layout = QtGui.QVBoxLayout()
+        layout.addWidget(self.gStackBar)
+        layout.addWidget(self.view)
+        self.setLayout(layout)
+        # Temporary dimension of the widget.
+        self.setGeometry(300, 300, 500, 500)
+
+    def addGuest(self):
+        print "Creating a row..."
+        self.model.insertRows(self.model.rowCount(), 1)
+        self.view.openPersistentEditor(
+                self.model.index(self.model.rowCount() - 1, 2))
+
+    def delGuest(self):
+        print "Deleting a list of selected rows"
+        selection = self.view.selectionModel()
+        l = reversed(list(skip_duplicates([item.row()
+                                           for item in selection.selectedIndexes()])))
+        for row in l:
+            self.model.removeRows(row, 1)
+
 
 # MAIN -------------------------------------------------------------------------
 def main():
@@ -192,6 +203,7 @@ def main():
     myWindow = Widget()
     myWindow.show()
     app.exec_()
+
 
 # MAIN--------------------------------------------------------------------------
 if __name__ == '__main__':
--- StackBar.py
+++ StackBar.py
@@ -11,6 +11,8 @@ from PyQt4 import QtGui
 # FUNCTIONS --------------------------------------------------------------------
 def showMessage(self, parent, title, text):
     QtGui.QMessageBox.information(parent, title, text)
+
+
 # CLASSES ----------------------------------------------------------------------
 
 class StackBar(QtGui.QWidget):
@@ -24,30 +26,32 @@ class StackBar(QtGui.QWidget):
         self.hBorder = 5
         self.btnGap = 5
         self.height = 35
-        self.initGrpButton(self.height,self.height)
-        self.setMinimumSize(0,self.height+self.hBorder*2)
+        self.initGrpButton(self.height, self.height)
+        self.setMinimumSize(0, self.height + self.hBorder * 2)
+
     def initGrpButton(self, width, height, text=''):
         self.menu = QtGui.QMenu(None)
         self.menu.ToolButtonPopupMode = 2
         self.groupBtn = QtGui.QPushButton(self)
         self.groupBtn.setText(text)
-        self.groupBtn.setGeometry(0,0,width, self.height)
+        self.groupBtn.setGeometry(0, 0, width, self.height)
         self.groupBtn.hide()
         self.groupBtn.setMenu(self.menu)
         for action in self.actList:
             self.menu.addAction(action)
             action.setVisible(False)
+
     def addItem(self, width, text, icon, txtOnIcon=False, tooltip=''):
         self.currentItem = len(self.btnList)
-        self.currentx = self.currentItem*self.btnGap + sum([i.size().width()
-            for i in self.btnList])
+        self.currentx = self.currentItem * self.btnGap + sum([i.size().width()
+                                                              for i in self.btnList])
         if txtOnIcon:
             txt = text
         else:
-            txt = ''#on ne met que l'icone.
-        self.btnList.append(QtGui.QPushButton(txt,  self))
+            txt = ''  # on ne met que l'icone.
+        self.btnList.append(QtGui.QPushButton(txt, self))
         self.fctList.append(lambda x: None)
-        self.actList.append(QtGui.QAction(QtGui.QIcon(icon),text, self.menu))
+        self.actList.append(QtGui.QAction(QtGui.QIcon(icon), text, self.menu))
         self.btnList[self.currentItem].move(self.currentx, self.hBorder)
         self.btnList[self.currentItem].setIcon(QtGui.QIcon(icon))
         self.btnList[self.currentItem].setFixedSize(width, self.height)
@@ -56,39 +60,41 @@ class StackBar(QtGui.QWidget):
         self.actList[self.currentItem].setVisible(False)
         self.nBtn = len(self.btnList)
         self.currentx += width
+
     def resizeEvent(self, e):
         if self.parent == None:
             S = self.size().width()
         else:
             S = self.parent.size().width()
-        self.groupBtn.move(S-self.btnGap-self.groupBtn.size().width(),
-                                                                 self.hBorder)
+        self.groupBtn.move(S - self.btnGap - self.groupBtn.size().width(),
+                           self.hBorder)
         if S < self.currentx:
             self.groupBtn.show()
-            self.btnList[self.nBtn-1].hide()
-            self.actList[self.nBtn-1].setVisible(True)
+            self.btnList[self.nBtn - 1].hide()
+            self.actList[self.nBtn - 1].setVisible(True)
         else:
             self.groupBtn.hide()
-            self.btnList[self.nBtn-1].show()
-            self.actList[self.nBtn-1].setVisible(False)
-        for i in range(self.nBtn-1):
-            xI = self.btnGap * (i+1) + sum([j.size().width()
-                                for j in self.btnList[:i+1]]) \
-                                + self.groupBtn.size().width()
+            self.btnList[self.nBtn - 1].show()
+            self.actList[self.nBtn - 1].setVisible(False)
+        for i in range(self.nBtn - 1):
+            xI = self.btnGap * (i + 1) + sum([j.size().width()
+                                              for j in self.btnList[:i + 1]]) \
+                 + self.groupBtn.size().width()
             if S < xI:
                 self.btnList[i].hide()
                 self.actList[i].setVisible(True)
             else:
                 self.btnList[i].show()
                 self.actList[i].setVisible(False)
+
     def setHeight(self, height):
-        self.setMinimumSize(0,height+self.hBorder*2)
+        self.setMinimumSize(0, height + self.hBorder * 2)
         for i in range(self.nBtn):
             self.btnList[i].setFixedHeight(height)
         self.groupBtn.setFixedHeight(height)
-        
+
     def setFunction(self, i, function):
-        if len(self.fctList) <i:
+        if len(self.fctList) < i:
             self.fctList.append(function)
         else:
             self.fctList[i] = function
@@ -105,7 +111,7 @@ class MainWindow(QtGui.QWidget):
         for i in range(10):
             width = random.randrange(25, 100, 1)
             c = random.choice([True, False])
-            self.widget.addItem(width,str(i), 'images/plus.png', c)
+            self.widget.addItem(width, str(i), 'images/plus.png', c)
         vbox = QtGui.QVBoxLayout()
         self.widget.setHeight(45)
         vbox.addWidget(self.widget)
@@ -120,6 +126,7 @@ def main():
     myWindow = MainWindow()
     myWindow.show()
     app.exec_()
+
 
 # MAIN--------------------------------------------------------------------------
 if __name__ == '__main__':
